f2999cfcb2efca8c216cca2a4dd0e740551ec0f6
asthanalab <aasthana@vt.edu>
2026-06-07 15:46:51 -0700

    Remove contributed from ICQC news

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v19';
+const staticCacheName = 'site-static-v20';
 const assets = [
     './',
     './index.html',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v65';
+const staticCacheName = 'site-static-v66';
 const assets = [
     './',
     './index.html',
